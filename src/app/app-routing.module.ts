8105169e7afcb7bdc47ba3e4b757a52f0cb6002e
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2021-05-25 17:26:34 +0200

    Login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'chats', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) }];
+const routes: Routes = [{ path: 'chats', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
