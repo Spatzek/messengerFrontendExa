597c344ee490a59925dc2b43c7a2863ce271f9cb
Jonas Spatzek <Jonas_spatzek@hotmail.com>
2021-05-26 16:49:46 +0200

    creat account

--- src/app/login/sign-up/sign-up.component.ts
+++ src/app/login/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {SignUpService} from './Shared/SignUp.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,16 +9,40 @@ import {SignUpService} from './Shared/SignUp.service';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-  message = new FormControl('');
+  // @ts-ignore
+  signupForm: FormGroup;
+
   messages: string[] = [];
 
-  constructor(private signUpService: SignUpService) {
+  constructor(private signUpService: SignUpService,
+              private formBuilder: FormBuilder,
+              private router: Router) {
   }
 
   ngOnInit(): void {
-    this.signUpService.listenForMessages()
-      .subscribe(message => {
-        this.messages.push(message);
-      });
+    this.signupForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(30)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(10)])],
+      password2: ['']
+    }, {validator: this.checkPasswords});
+  }
+
+  checkPasswords(group: FormGroup): any {
+    const pass = group.get('password')?.value;
+    const pass2 = group.get('password2')?.value;
+    return pass === pass2 ? null : { notSame: true} ;
+  }
+
+  get f(): any{
+    return this.signupForm.controls;
+  }
+
+  signUp(): void {
+    if (this.signupForm.invalid){
+      return;
+    }
+
+    console.log(this.signupForm.value.username, this.signupForm.value.password);
+   //  this.router.navigate(['chats']);
   }
 }
